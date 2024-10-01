7d6ed76d5f92254264e39e9a4f3d9f8835c913c5
abhisheksharma <abhisheksharma131098@gmail.com>
2024-10-01 19:23:51 +0530

    created to-do app with PyQt5

--- gui-with-pyqt5/gui/app.py
+++ gui-with-pyqt5/gui/app.py
@@ -3,12 +3,16 @@ from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
 from main_window import Ui_MainWindow
 from dialog import Ui_Dialog
+from stylesheets import main_style_sheet
+from stylesheets import dialog_style_sheet
 
 class Dialog(QDialog):
     def __init__(self, parent=None):
         super(Dialog, self).__init__(parent)
         self.ui = Ui_Dialog()
         self.ui.setupUi(self)
+        #stylesheet
+        self.setStyleSheet(dialog_style_sheet)
 
 class MainWindow(QMainWindow, Ui_MainWindow):
     def __init__(self, parent=None):
@@ -16,9 +20,37 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         # vip
         self.setupUi(self)
         self.pushButton.clicked.connect(self.add_stuff)
+        self.done = []
+        self.not_done = []
+
+        #done button
+        self.pushButton_3.clicked.connect(self.do_task)
+        
+        #undone button
+        self.pushButton_2.clicked.connect(self.undo_task)
+
+        #styleshet
+        self.setStyleSheet(main_style_sheet)
+
+    def add_task(self, task):
+        if bool(task) != False:
+            self.remainingList.addItem(task)
+
+    def do_task(self):
+        task = self.remainingList.takeItem(self.remainingList.currentRow())
+        if bool(task) != False:
+            self.finishedList.addItem(task)
+
+    def undo_task(self):
+        task = self.finishedList.takeItem(self.finishedList.currentRow())
+        if bool(task) != False:
+            self.remainingList.addItem(task)
 
     def add_stuff(self):
         dlg = Dialog()
+        dlg.ui.buttonBox.accepted.connect(
+            lambda: self.add_task(dlg.ui.lineEdit.text())
+        )
         dlg.exec()
 
 app = QApplication([])
